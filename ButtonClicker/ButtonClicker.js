b624a77533e1f7dd3ffa8e0e3efd9d5d2a1f075a
m-smith15 <mpsmith1215@gmail.com>
2022-06-26 16:38:29 -0400

    ButtonClicker commit

---
+++ ButtonClicker/ButtonClicker.js
@@ -0,0 +1,9 @@
+function loginClick(element){
+    element.innerText = "Logout";
+}
+function hideAddDef(element){
+    element.remove();
+}
+function likeClicked(){
+    alert('Like was clicked!');
+}
